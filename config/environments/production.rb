a21dc1922a73fbe8fd7a677736a442791eccddd3
pirangabaneto <pirangabaneto@gmail.com>
2019-11-27 02:20:42 -0300

    testando integracao heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "teste_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Sistema-Gerenciador-de-Pedidos-de-Restaurantes_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
